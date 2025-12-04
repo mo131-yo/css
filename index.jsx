c2deaeb98ce571c7138cc414eb43bedfde7d898d
Munkh-Orgil <munhorgilmunhtsetseg333@gmail.com>
2025-12-04 13:48:17 +0800

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,22 @@
+import { Lorem } from "@/components/Lorem";
+import {Twice} from "@/components/Twice";
+import {Logo} from "@/components/Logo";
+import {Experience} from "@/components/Experience";
+import {Work} from "@/components/Work";
+import {Touch} from "@/components/Touch";
+import {Year} from "@/components/Year";
+export const Home =()=>{
+  return (
+<div>
+  <Lorem />
+  <Twice />
+  <Logo />
+  <Experience />
+  <Work />
+  <Touch />
+  <Year />
+</div>
+  );
+};
+
+export default Home;
