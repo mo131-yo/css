bfbacc0668a1cdd254b5beaddfc09408c63f6cba
Munkh-Orgil <munhorgilmunhtsetseg333@gmail.com>
2025-12-04 13:44:07 +0800

    Add files via upload

---
+++ Year.jsx
@@ -0,0 +1,8 @@
+export const Year=({})=>{
+    return(
+        <div className="flex flex-row p-24 justify-center gap-2">
+            <img src="/icond.png" alt="icond w-4 h-4" />
+            <p className="font-normal text-[14px] text-#4B5563">2024 | Greetings with ❤️️ from Ulaanbaatar</p>
+        </div>
+    )
+}
